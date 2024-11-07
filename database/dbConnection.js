49ef23cd6e5188ace734103f6c6024d40331bdae
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2024-11-07 05:33:19 +0200

    create server and connect with database

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 const dbconnection = async () => {
     try {
         await mongoose.connect("mongodb://127.0.0.1:27017/prescripto");
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import dbconnection from './database/dbConnection';
+import dbconnection from './database/dbConnection.js';
 const app = express();
 
 dbconnection()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-const port = process.env.PORT || 300
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
