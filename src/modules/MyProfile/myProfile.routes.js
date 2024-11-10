dbe0f4fd0916752a817797eec50e62caf03323da
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2024-11-10 19:04:02 +0200

    create myProfile api route to update doctor, manager and user profile

--- src/modules/MyProfile/myProfile.routes.js
+++ src/modules/MyProfile/myProfile.routes.js
@@ -1,13 +1,17 @@
 import {Router} from "express"
 import UpdateUserActivity from "../../../utils/UpdateUserActivity";
-import { deleteProfile, getMyProfile } from "./myProfile.controller";
+import { deleteMyProfile, getMyProfile, updateMyDoctorProfile, updateMyManagerProfile, updateMyUserProfile } from "./myProfile.controller";
 import roleAccess from "../../middlewares/roleAccess";
 
 const myProfileRouter = Router();
 
 myProfileRouter.use(protectAuth, UpdateUserActivity)
+
+myProfileRouter.patch("/updateDoctorProfile", roleAccess("doctor"), updateMyDoctorProfile)
+myProfileRouter.patch("/updateUserProfile", roleAccess("user"), updateMyUserProfile)
+myProfileRouter.patch("/updateManagerProfile", roleAccess("manager"), updateMyManagerProfile)
     
 myProfileRouter.route("/")
     .get(getMyProfile)
-    .delete(roleAccess("user"), deleteProfile)
+    .delete(roleAccess("user"), deleteMyProfile)
 export default myProfileRouter;
