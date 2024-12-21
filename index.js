839cdbed6a57f3053da3b38d42023c718b033016
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2024-12-21 16:27:57 +0200

    update uploaded images url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use("/uploads", express.static("public"));
 // app.use("/uploads", express.static("uploads/doctors"))
 // app.use("/uploads", express.static("uploads/users"))
 // app.use("/uploads", express.static("uploads/managers"))
