93e4c58b541c53f9e4af7d160193e958427a6425
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2024-12-21 15:57:54 +0200

    update uploaded images url

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/uploads/doctors", express.static("uploads/doctors"))
 // app.use("/uploads", express.static("uploads/users"))
 // app.use("/uploads", express.static("uploads/managers"))
 
 app.use(cors())
 app.use(UpdateUserActivity)
 app.use(deleteExpireAppointments)
+
+app.use("/uploads/doctors", express.static("uploads/doctors"))
 
 dbconnection()
 Bootstrap(app)
